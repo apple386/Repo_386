a3e93ed9e09a29410e6c961a0667a930fddb0091
Alexey_Nikulin <apple386@mail.ru>
2020-11-28 12:05:31 +0300

    Lesson3

---
+++ src/Nikulin/lesson3/Fruit.java
@@ -0,0 +1,5 @@
+package Nikulin.lesson3;
+
+public abstract class Fruit {
+    abstract float getWeight();
+}
---
+++ src/Nikulin/lesson3/Orange.java
@@ -0,0 +1,8 @@
+package Nikulin.lesson3;
+
+public class Orange extends Fruit {
+    @Override
+    float getWeight() {
+        return 1.5f;
+    }
+}

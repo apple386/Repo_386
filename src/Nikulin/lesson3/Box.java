7afe324d2e36475750114e6b0f6e35b830e20429
Alexey_Nikulin <apple386@mail.ru>
2020-11-30 12:45:00 +0300

    lesson3

--- src/Nikulin/lesson3/Box.java
+++ src/Nikulin/lesson3/Box.java
@@ -6,28 +6,31 @@ import java.util.Arrays;
 public class Box<T extends Fruit> {
     private ArrayList<T> box = new ArrayList<>();
 
-    public Box(){
+    public Box() {
 
     }
 
-    public float getWeight(){
+    public float getWeight() {
         float weight = 0.0f;
 
-        for(T o : box){
+        for (T o : box) {
             weight += o.getWeight();
-        }return weight;
+        }
+        return weight;
     }
+
     public boolean compare(Box anotherBox) {
-        if(getWeight() == anotherBox.getWeight()) return true;
+        if (getWeight() == anotherBox.getWeight()) return true;
         return false;
     }
-    public void pourTo(Box <T>anotherBox){
+
+    public void pourTo(Box<T> anotherBox) {
         anotherBox.box.addAll(box);
         box.clear();
     }
 
-    public void addFruit(T fruit, int amount){
-        for(int i=0;i<amount;i++){
+    public void addFruit(T fruit, int amount) {
+        for (int i = 0; i < amount; i++) {
             box.add(fruit);
         }
     }
